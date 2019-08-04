fae0863b6aef35cffa413d20de0b651ecb24cb3c
Keitaro Hashimoto <kaminomisosiru.414@outlook.com>
2019-08-04 23:55:28 +0900

    Change submission counter
    Count the number of submissions using a static class variable.

--- ac-core/src/main/java/es/ucm/fdi/ac/Analysis.java
+++ ac-core/src/main/java/es/ucm/fdi/ac/Analysis.java
@@ -63,6 +63,7 @@ public class Analysis implements XMLSerializable {
 	private static final Logger log = Logger.getLogger(Analysis.class);
 
 	private static final String VERSION_STRING = "2.0";
+	private static int subCounter = 0;
 
 	/** Set of sources that are being analyzed. */
 	private SourceSet sourceSet;
@@ -121,13 +122,13 @@ public class Analysis implements XMLSerializable {
 
 		HashMap<String, Submission> unique = new HashMap<>();
 		idsToSubs.clear();
-		int i = 0;
+        subCounter = 0;
 		for (FileTreeNode dn : root.getChildren()) {
-			addSubmissionRecursive(dn, unique, i);
+			addSubmissionRecursive(dn, unique);
 		}
 
 		subs = new Submission[unique.size()];
-        i = 0;
+        int i = 0;
 		for (Submission s : unique.values()) {
             subs[i++] = s;
 			idsToSubs.put(s.getId(), s);
@@ -135,17 +136,16 @@ public class Analysis implements XMLSerializable {
 	}
 
 	private void addSubmissionRecursive(FileTreeNode tn,
-			HashMap<String, Submission> unique, int i) {
+			HashMap<String, Submission> unique) {
 		if (tn.getFile().isDirectory()) {
 			for (FileTreeNode fn : tn.getLeafChildren()) {
-				addSubmissionRecursive(fn, unique, i);
-				i++;
+				addSubmissionRecursive(fn, unique);
 			}
 		} else {
 			if (tn.getFile().getName().startsWith(".")) // skip hidden file such as .DS_Store
 				return;
 			Submission s = new Submission(tn.getLabel(), tn.getPath(), 0);
-			log.info("created sub " + s.getId());
+			log.info("created sub " + s.getId() + s.getInternalId());
 			for (FileTreeNode fn : tn.getLeafChildren()) {
 				log.debug(" - " + fn.getFile().getName());
 				s.addSource(fn.getFile());
@@ -153,7 +153,8 @@ public class Analysis implements XMLSerializable {
 
 			if (!unique.containsKey(s.getHash())) {
 				unique.put(s.getHash(), s);
-				s.setInternalId(i);
+				s.setInternalId(subCounter++);
+				log.info("sub internal id " + s.getInternalId());
 			} else {
 				Submission p = unique.get(s.getHash());
 				log.warn("Detected EXACT duplicate: " + s.getHash() + "\n"
