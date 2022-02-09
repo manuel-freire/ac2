fb1611dc2d5265be1614ae1f4a8c8026f5d4e50f
Manuel Freire <manuel.freire@gmail.com>
2022-02-09 12:10:02 +0100

    Filter logs by priority
    makes warnings easier to see

--- ac-ui/src/main/java/es/ucm/fdi/ac/Main.java
+++ ac-ui/src/main/java/es/ucm/fdi/ac/Main.java
@@ -22,37 +22,24 @@
  */
 package es.ucm.fdi.ac;
 
+import static es.ucm.fdi.util.I18N.m;
+
+import java.awt.Color;
+import java.io.File;
+import java.util.Locale;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import es.ucm.fdi.ac.extract.ZipSelectionPanel;
 import es.ucm.fdi.ac.gui.ACVersion;
 import es.ucm.fdi.ac.gui.MainGui;
 import es.ucm.fdi.util.I18N;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.Filter;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.StringLayout;
-import org.apache.logging.log4j.core.appender.ConsoleAppender;
-import org.apache.logging.log4j.core.appender.WriterAppender;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.LoggerConfig;
-
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
-
-import static es.ucm.fdi.util.I18N.m;
-
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Locale;
 
 /**
  * Main graphical entry-point.
@@ -61,80 +48,17 @@ import java.util.Locale;
  * @author  mfreire
  */
 public class Main extends javax.swing.JFrame implements
-		ZipSelectionPanel.SelectionListener {
+		ZipSelectionPanel.SelectionListener, LogPanel.UnreadLineListener {
 
 	private Logger log = LogManager.getLogger(Main.class);
 
 	private ZipSelectionPanel zsp;
-	private JTextPane logPane;
 	private JTabbedPane tabs;
+
 	private int unreadLogLines = 0;
-	private static SimpleAttributeSet highlighted;
 
-	private void initLoggingAndStyles() {
-		highlighted = new SimpleAttributeSet();
-		StyleConstants.setBold(highlighted, true);
-
-		addAppender(new LogWriter(), "TabbedLogAppender");
-	}
-
-	void addAppender(final Writer writer, final String writerName) {
-		final LoggerContext context = LoggerContext.getContext(false);
-		final Configuration config = context.getConfiguration();
-		ConsoleAppender prev = config.getAppender("STDOUT");
-		final Appender appender = WriterAppender.createAppender(
-				(StringLayout) prev.getLayout(), null, writer, writerName,
-				false, true);
-		appender.start();
-		config.addAppender(appender);
-		updateLoggers(appender, config);
-	}
-
-	private void updateLoggers(final Appender appender,
-			final Configuration config) {
-		final Level level = null;
-		final Filter filter = null;
-		for (final LoggerConfig loggerConfig : config.getLoggers().values()) {
-			loggerConfig.addAppender(appender, level, filter);
-		}
-		config.getRootLogger().addAppender(appender, level, filter);
-	}
-
-	private class LogWriter extends Writer {
-		@Override
-		public void write(char[] cbuf, int off, int len) throws IOException {
-			String message = new String(cbuf, off, len);
-			String title = "???";
-			final String SEPARATOR = "::";
-			if (message.substring(0, Math.min(message.length(), 64)).contains(
-					SEPARATOR)) {
-				title = message.substring(0, message.indexOf(SEPARATOR));
-				message = message
-						.substring(title.length() + SEPARATOR.length());
-			}
-
-			String[] lines = message.split("\n");
-			StringBuilder retained = new StringBuilder();
-			for (String line : lines) {
-				if (!line.trim().startsWith("at java")) {
-					retained.append(line).append("\n");
-				}
-			}
-
-			logEvent(title, retained.toString());
-			updateUnreadLogLines();
-		}
-
-		@Override
-		public void flush() throws IOException {
-		}
-
-		@Override
-		public void close() throws IOException {
-		}
-	}
-
-	private void updateUnreadLogLines() {
+	@Override
+	public void incrementUnreadLines() {
 		unreadLogLines++;
 		if (unreadLogLines == 0) {
 			tabs.setBackgroundAt(1, tabs.getBackgroundAt(0));
@@ -144,6 +68,12 @@ public class Main extends javax.swing.JFrame implements
 		tabs.setTitleAt(1, m("Tabs.Log") + " (" + unreadLogLines + ")");
 	}
 
+	@Override
+	public void resetUnreadLines() {
+		unreadLogLines = -1;
+		incrementUnreadLines();
+	}
+
 	/**
 	 * Creates new UI
 	 */
@@ -151,18 +81,15 @@ public class Main extends javax.swing.JFrame implements
 		zsp = new ZipSelectionPanel(this);
 		tabs = new JTabbedPane();
 		tabs.add(m("Tabs.Extract"), zsp);
-		logPane = new JTextPane();
-		logPane.setEditable(false);
-		tabs.add(m("Tabs.Log"), new JScrollPane(logPane));
+
+		tabs.add(m("Tabs.Log"), new JScrollPane(new LogPanel(this)));
 		tabs.addChangeListener(new ChangeListener() {
 
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				unreadLogLines = -1;
-				updateUnreadLogLines();
+				resetUnreadLines();
 			}
 		});
-		initLoggingAndStyles();
 		getContentPane().add(tabs);
 
 		setSize(1200, 800);
@@ -200,16 +127,6 @@ public class Main extends javax.swing.JFrame implements
 			main.loadSources(ss);
 		} catch (Exception e) {
 			log.warn("Error loading selected sources", e);
-		}
-	}
-
-	public void logEvent(String eventTitle, String eventText) {
-		try {
-			Document doc = logPane.getDocument();
-			doc.insertString(doc.getLength(), eventTitle, highlighted);
-			doc.insertString(doc.getLength(), eventText, null);
-		} catch (BadLocationException e) {
-			log.warn("Bad insertion position", e);
 		}
 	}
 }
